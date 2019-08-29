22afcd880fc1d4526babe0d3173b35de0344cd6d
Huzi <aa407766499@outlook.com>
2019-08-29 14:22:02 +0800

    ContextResource 中文注释

--- spring-core/src/main/java/org/springframework/core/io/ContextResource.java
+++ spring-core/src/main/java/org/springframework/core/io/ContextResource.java
@@ -18,7 +18,9 @@ package org.springframework.core.io;
 
 /**
  * Extended interface for a resource that is loaded from an enclosing
+ * 扩展接口，从封装的上下文加载的资源，比如从ServletContext，但是也可以是类路径
  * 'context', e.g. from a {@link javax.servlet.ServletContext} but also
+ * 或者文件系统相对路径（指定的路径没有明确的前缀，因此应用本地ResourceLoader上下文的相对路径）
  * from plain classpath paths or relative file system paths (specified
  * without an explicit prefix, hence applying relative to the local
  * {@link ResourceLoader}'s context).
@@ -31,7 +33,9 @@ public interface ContextResource extends Resource {
 
 	/**
 	 * Return the path within the enclosing 'context'.
+	 * 返回封装的上下文中的路径
 	 * <p>This is typically path relative to a context-specific root directory,
+	 * 通常是上下文指定根目录的相对路径。比如ServletContext根路径或者PortletContext根路径
 	 * e.g. a ServletContext root or a PortletContext root.
 	 */
 	String getPathWithinContext();

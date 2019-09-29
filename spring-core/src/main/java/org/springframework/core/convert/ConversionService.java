3ee5207eb2fd5fdec185c1ba6b29400e1d58d958
Huzi <aa407766499@outlook.com>
2019-09-29 20:56:59 +0800

    DI getBean()流程部分中文注释

--- spring-core/src/main/java/org/springframework/core/convert/ConversionService.java
+++ spring-core/src/main/java/org/springframework/core/convert/ConversionService.java
@@ -20,7 +20,7 @@ import org.springframework.lang.Nullable;
 
 /**
  * A service interface for type conversion. This is the entry point into the convert system.
- * 类型转换的服务接口。这是转换系统的入口。
+ * 类型转换的服务接口。这是转换系统的入口。使用该系统调用convert方法执行线程安全的类型转换。
  * Call {@link #convert(Object, Class)} to perform a thread-safe type conversion using this system.
  *
  * @author Keith Donald
@@ -31,10 +31,14 @@ public interface ConversionService {
 
 	/**
 	 * Return {@code true} if objects of {@code sourceType} can be converted to the {@code targetType}.
+	 * 如果sourceType的对象能被转换为targetType的对象返回true。
 	 * <p>If this method returns {@code true}, it means {@link #convert(Object, Class)} is capable
+	 * 如果该方法返回true，意味着convert方法能够将sourceType的实例转换为targetType的实例。
 	 * of converting an instance of {@code sourceType} to {@code targetType}.
 	 * <p>Special note on collections, arrays, and maps types:
+	 * 对于集合类型，数组类型，map类型：集合，数组，map类型之间的转换，该方法会返回true，即使转换调用会产生
 	 * For conversion between collection, array, and map types, this method will return {@code true}
+	 * ConversionException如果底层的元素不能转换。调用者需要处理异常情况，在处理集合和map时。看
 	 * even though a convert invocation may still generate a {@link ConversionException} if the
 	 * underlying elements are not convertible. Callers are expected to handle this exceptional case
 	 * when working with collections and maps.
